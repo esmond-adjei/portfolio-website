44ae87de7db44f135c7ff4ba3843058cdb060f6c
Esmond Adjei <xmongram@gmail.com>
2023-05-10 14:13:35 +0000

    modified:   portfolio-front/package-lock.json 	modified:   portfolio-front/package.json 	modified:   portfolio-front/src/components/Content.js 	modified:   portfolio-front/src/style/component-styles/Content.css 	modified:   portfolio-front/src/style/component-styles/Footer.css

--- portfolio-front/src/components/Content.js
+++ portfolio-front/src/components/Content.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+// import { Container, Row, Col } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import Profile from './Profile';
 // import Projects from './Projects';
 // import SkillsTools from './SkillsTools';
@@ -8,34 +10,44 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 function MainContent() {
     return (
-        <Container fluid>
-            <Row>
-                <Col md={3}>
-                    <p>Profile</p>
-                </Col>
-                <Col md={9}>
-                    <Row>
-                        <Col>
-                            <p>Projects </p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={6}>
-                            <p>Skills and Tools</p>
-                        </Col>
-                        <Col md={6}>
-                            <p>Blog Posts</p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <p>Interests</p>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
+        <div className='main_content'>
+            <div className='profile__column'>
+                <p>profile content</p>
+            </div>
+
+            <div className='content__column'>
+                <p>main content</p>
+            </div>
+        </div>
     );
 };
 
 export default MainContent;
+
+        // <Container fluid>
+        //     <Row>
+        //         <Col md={3}>
+        //             <p>Profile</p>
+        //         </Col>
+        //         <Col md={9}>
+        //             <Row>
+        //                 <Col>
+        //                     <p>Projects </p>
+        //                 </Col>
+        //             </Row>
+        //             <Row>
+        //                 <Col md={6}>
+        //                     <p>Skills and Tools</p>
+        //                 </Col>
+        //                 <Col md={6}>
+        //                     <p>Blog Posts</p>
+        //                 </Col>
+        //             </Row>
+        //             <Row>
+        //                 <Col>
+        //                     <p>Interests</p>
+        //                 </Col>
+        //             </Row>
+        //         </Col>
+        //     </Row>
+        // </Container>
