3af5252eef475e1349b991da1ceaa6d40c03a18a
esmond-adjei <xmongram@gmail.com>
2023-06-05 15:42:07 +0000

    modified some files

--- portfolio-front/src/components/Footer.js
+++ portfolio-front/src/components/Footer.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import EmailForm from './microcomponents/emailform';
+
 
 function Footer() {
     return (
         <footer>
             <div className="footer__content">
-                <p>Copyright © 2023</p>
-                <ul className="footer__links">
-                    <li><a href="/#">Home</a></li>
-                    <li><a href="/#">About</a></li>
-                    <li><a href="/#">Contact</a></li>
-                </ul>
+                <div className='footer__info'>
+                    {/* email forms */}
+                    <EmailForm />
+
+                    {/* footer links */}
+                    <ul className="footer__links">
+                        <li><a href="/#">Home</a></li>
+                        <li><a href="/#">About</a></li>
+                        <li><a href="/#">Contact</a></li>
+                    </ul>
+                </div>
+                <p className='footer_copyright'>Copyright © 2023</p>
             </div>
         </footer>
     );
---
+++ portfolio-front/src/components/microcomponents/emailform.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './micro.css';
+
+function EmailForm() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle form submission logic here
+    };
+
+    return (
+            <form className="email-form" onSubmit={handleSubmit}>
+                <h2>Send me a message</h2>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" required placeholder='enter your name...'/>
+
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required placeholder='example@gmail.com'/>
+
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" rows="4" required placeholder='Hi Esmond, I would like to ...'></textarea>
+
+                <button type="submit">Send</button>
+            </form>
+     );
+}
+
+export default EmailForm;
